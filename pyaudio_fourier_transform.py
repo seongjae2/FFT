35896c4f1a2d3a744e74e7478a54ed90070fe816
tjdwo1105 <percy9875@gmail.com>
2019-06-08 15:59:46 +0900

    tuner finished
    19.06.08
    
    물리 탐구

--- pyaudio_fourier_transform.py
+++ pyaudio_fourier_transform.py
@@ -6,15 +6,18 @@ import time
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.set_xlim((0, 2000))  # x축
+ax.set_xlim((0, 3000))  # x축
 ax.set_xlabel('Freguency')
-ax.set_ylim((0, 2500))  # y축
+ax.set_ylim((0, 3000))  # y축
 ax.set_ylabel('Amplitude')
+
 line, = ax.plot([], [], c='k', lw=1)
 
 NOTE_MIN = 50  # D3
 NOTE_MAX = 84  # C6
+limit_a = 1700
 
+NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
 
 # note <-> frequency
 
@@ -27,14 +30,13 @@ def number_to_freq(n):
 
 
 def note_name(n):
-    return NOTE_NAMES[n % 12] + str(n / 12 - 1)
-
+    return NOTE_NAMES[n % 12]
 
 fmin = round(number_to_freq(NOTE_MIN))
 fmax = round(number_to_freq(NOTE_MAX))
-NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
 
-
+print(NOTE_NAMES)
+print()
 
 #matplot lib 각 프레임에 적용될 배경 초기화 작업
 
@@ -46,21 +48,22 @@ def init():
 def animate(i):
     data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
     n = len(data)
-    x = np.linspace(0, 44100 / 2, n / 2)    # 균일한 배열 생성 (start, end, num-points)
+    x = np.linspace(0, 44100/2, n/2)    # 균일한 배열 생성 (start, end, num-points)
     y = np.fft.fft(data) / n
+    y = y[range(int(n/2))]
     y = np.absolute(y)
-    y = y[range(int(n / 2))]                # 해결중... 전혀 안됨 아래부터
-    max_f = np.argmax(y[fmin:fmax])+fmin    # 원하는 진동수 범위 중 최대값을 가지는 진동수
-    max_a = np.max(y[fmin:fmax])
-    if (max_a > 80):
-        print(np.argmax(y[fmin:fmax]))
-        n0 = int(round(69 + 12 * np.log2(max_f / 440.0)))  # 진동수 -> note number
-        # print(n0, NOTE_NAMES[n0 % 12], str(n0 / 12 - 1))   # note number, notes, ?
+    max_f = np.argmax(y)*(44100/n)   # 원하는 진동수 범위 중 최대값을 가지는 진동수
+    max_a = np.max(y)
+    if max_a > limit_a:
+        note = freq_to_number(max_f)
+        note0 = int(round(69 + 12 * np.log2(max_f / 440.0)))
+        print('freq: {:6.1f} Hz  \ta: {:5.2f}   \tnote: {:>3s} {}    \tgap: {:+1.3f}'.format(
+            max_f, max_a, note_name(note0), note0 , (note - note0)))
     line.set_data(x, y)
     return line,
 
 
-CHUNK = 1024    # (randomly chosen)number of frames that the signals are split into
+CHUNK = 2048    # (randomly chosen)number of frames that the signals are split into
 FORMAT = pyaudio.paInt16
 CHANNELS = 2    # Each frame will have 2 samples, 2 bytes
 RATE = 44100    # the number of frames per second
@@ -80,60 +83,3 @@ animation = animation.FuncAnimation(fig, animate, init_func=init,
 
 
 plt.show()
-
-
-# printing notes
-if 0:
-    NOTE_MIN = 50       # D3
-    NOTE_MAX = 84       # C6
-    FSAMP = 22050       # Sampling frequency in Hz
-    FRAME_SIZE = 2048   # How many samples per frame?
-    FRAMES_PER_FFT = 16 # FFT takes average across how many frames?
-
-    SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT
-    FREQ_STEP = float(FSAMP)/SAMPLES_PER_FFT
-
-    NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
-
-
-    # note <-> frequency
-
-    def freq_to_number(f):
-        return 69 + 12 * np.log2(f / 440.0)
-
-
-    def number_to_freq(n):
-        return 440 * 2.0 ** ((n - 69) / 12.0)
-
-
-    def note_name(n):
-        return NOTE_NAMES[n % 12] + str(n / 12 - 1)
-
-
-    def note_to_fftbin(n):
-        return number_to_freq(n) / FREQ_STEP
-
-
-    imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
-    imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
-
-    buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
-    num_frames = 0
-
-
-    # As long as we are getting data:
-    while stream.is_active():
-
-        # Get frequency of maximum response in range
-        freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
-
-        # Get note number and nearest note
-        n = freq_to_number(freq)
-        n0 = int(round(n))
-
-        # Console output once we have a full buffer
-        num_frames += 1
-
-        if num_frames >= FRAMES_PER_FFT:
-            print('freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(
-                freq, note_name(n0), n-n0))
